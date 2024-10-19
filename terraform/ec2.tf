01426fdd8d981cefec96ab3255c2f47e180fd633
rashadahmed424 <99238868+rashadahmed424@users.noreply.github.com>
2024-10-19 17:12:54 +0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@
 # Define the EC2 instance
 resource "aws_instance" "jenkins_server" {
   ami           ="ami-0182f373e66f89c85"
-  instance_type = "t2.micro"               
+  instance_type = "t2.micro"         # used due to free tier account, needs more resources
   vpc_security_group_ids = [aws_security_group.my_jenkins_security_group.id]
   subnet_id     = aws_subnet.my_subnet.id
   key_name = aws_key_pair.my_key_pair.key_name
